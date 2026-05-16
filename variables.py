a74c26103d079b9307cda5509b82aa8e062b29f8
noorulhuda0604-hue <noorulhuda0604@gmail.com>
2026-05-16 20:41:03 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,23 @@
+name="Noor"
+age=20
+print(name)
+print(age)
+
+name = "Ahmad"
+city = "Gujrat"
+print("Name:", name)
+print("City:", city)
+
+# text and Variable 
+name="Eisha"
+age=19
+city="Gujrat"
+print('My name is',name)
+print('My age is',age)
+print("I live in",city)
+
+# Text formatting (f-string)
+name="Walli"
+age=16
+city="Lahore"
+print(f"My name is {name}, I am {age} years old and I live in { city}")
